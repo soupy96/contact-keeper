86fb1704230fa5dc95f910927cb1a4ae43b2a11f
Michael Campbell <michael.campbell14@gmail.com>
2020-08-28 15:08:57 -0400

    lesson 45, authenticate route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/Users');
 
