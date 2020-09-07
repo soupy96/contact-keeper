45168717a107616f21abbc416face007c8f11678
Michael Campbell <michael.campbell14@gmail.com>
2020-09-07 13:21:51 -0400

    lesson 55, delete contact from UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,9 @@ import {
     }
 
     // delete contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id })
+    }
 
     // set current contact
 
@@ -62,7 +65,8 @@ import {
     return (
         <ContactContext.Provider value={{
             contacts: state.contacts,
-            addContact
+            addContact,
+            deleteContact
         }}>
             { props.children }
         </ContactContext.Provider>
