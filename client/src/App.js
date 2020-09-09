ff66d3deaaab4ca3726e4eb343a840d32f36fc4d
Michael Campbell <michael.campbell14@gmail.com>
2020-09-09 13:46:17 -0400

    lesson 62, alert context, state & component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,38 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home.js'
 import About from './components/pages/About.js'
 import Register from './components/auth/Register.js'
 import Login from './components/auth/Login.js'
+import Alerts from './components/layout/Alerts.js'
+
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
+import AletState from './context/alert/AlertState'
+
+import './App.css';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AletState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AletState>
       </ContactState>
     </AuthState>
   );
