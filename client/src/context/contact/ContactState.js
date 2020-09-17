21786a576a0577c38ccb26c9821344546adc5699
Michael Campbell <michael.campbell14@gmail.com>
2020-09-17 13:34:09 -0400

    lesson 71, update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -52,21 +52,37 @@ import {
         } catch (err) {
             dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
         }
-
     }
-
+    
     // delete contact
     const deleteContact = async id => {
         try {
             await axios.delete(`/api/contacts/${id}`)
-
+            
             dispatch({ type: DELETE_CONTACT, payload: id })
         } catch (err) {
             dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
         }
     }
+    
+    // update contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Context-Type': 'application/json'
+            }
+        }
 
-     // clear contacts
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
+
+            dispatch({ type: UPDATE_CONTACT, payload: res.data })
+        } catch (err) {
+            dispatch({ type: CONTACT_ERROR, payload: err.response.msg })
+        }
+    }
+
+    // clear contacts
      const clearContacts = () => {
          dispatch({ type: CLEAR_CONTACTS })
      }
@@ -81,10 +97,6 @@ import {
         dispatch({ type: CLEAR_CURRENT })
     }
 
-    // update contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact })
-    }
 
     // filter contacts
     const filterContacts = text => {
